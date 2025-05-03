0b1a325ab017e8ded17a79338afba348dc653532
azizziy <usmonovazizbek045@gmail.com>
2025-05-03 18:25:31 +0500

    test husky 5

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,10 +4,10 @@ import { getLocale } from 'next-intl/server';
 
 export default function Home() {
   return (
-    <>
+    <div>
       <h1>{formatDate.relative(Date.now())}</h1>
       <h1>{getLocale()}</h1>
       <Welcome />
-    </>
+    </div>
   );
-};;
+}
